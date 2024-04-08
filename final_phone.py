e149961584c711ddf5605cf8fe42ad105c80d0ca
Дмитрий <osdal67@gmail.com>
2024-04-08 14:51:51 +0300

    222

--- final_phone.py
+++ final_phone.py
@@ -1,16 +1,27 @@
 import pandas as pd
 
-# Чтение данных из файла final_phone.csv
-final_phone_data = pd.read_csv('final_phone.csv')
+# Чтение данных из файла final.csv
+final_data = pd.read_csv('final.csv')
 
-# Чтение данных из файла friends_phone_barkov_comma.csv
-friends_data = pd.read_csv('friends_phone_barkov_comma.csv')
+# Проверка наличия столбца 'ID_friends' в файле final.csv
+if 'ID_friends' not in final_data.columns:
+    print("Столбец 'ID_friends' отсутствует в файле 'final.csv'.")
+    print("Пожалуйста, убедитесь, что файл содержит нужные столбцы.")
+    exit()
 
-# Выполнение объединения по столбцам friends и ID
-final_phone_data = final_phone_data.merge(friends_data[['ID', 'name', 'phone']], left_on='friends', right_on='ID', how='left')
+# Итерация по каждой ячейке столбца ID_friends
+for index, row in final_data.iterrows():
+    id_fr_value = row['ID_friends']
 
-# Удаление лишнего столбца 'ID'
-final_phone_data.drop(columns=['ID'], inplace=True)
+    # Проверка наличия значения ID_friends
+    if pd.notna(id_fr_value):
+        # Итерация по всем строкам и сравнение значения ID_friends со значениями в столбце friends
+        for idx, r in final_data.iterrows():
+            friends_value = r['friends']
+            if id_fr_value == friends_value:
+                # Копирование значения friends в столбец id_fr
+                final_data.at[index, 'id_fr'] = id_fr_value
+                break  # Прекращаем цикл после первого совпадения
 
-# Сохранение результата в файл final_phone.csv
-final_phone_data.to_csv('final_phone.csv', index=False)
+# Сохранение результата в файл final.csv
+final_data.to_csv('final.csv', index=False)

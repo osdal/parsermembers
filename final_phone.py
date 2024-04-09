ceeeb5d0581e5504c6f432b3ba6fb25c3caeef08
Дмитрий <osdal67@gmail.com>
2024-04-09 09:03:01 +0300

    212

--- final_phone.py
+++ final_phone.py
@@ -3,25 +3,35 @@ import pandas as pd
 # Чтение данных из файла final.csv
 final_data = pd.read_csv('final.csv')
 
-# Проверка наличия столбца 'ID_friends' в файле final.csv
-if 'ID_friends' not in final_data.columns:
-    print("Столбец 'ID_friends' отсутствует в файле 'final.csv'.")
+# Проверка наличия столбцов 'name_friend' и 'phone' в файле final.csv
+if 'name_friend' not in final_data.columns or 'phone' not in final_data.columns:
+    print("Столбцы 'name_friend' или 'phone' отсутствуют в файле 'final.csv'.")
     print("Пожалуйста, убедитесь, что файл содержит нужные столбцы.")
     exit()
 
-# Итерация по каждой ячейке столбца ID_friends
+# Итерация по каждой ячейке столбца name_friend
 for index, row in final_data.iterrows():
-    id_fr_value = row['ID_friends']
+    name_fr_value = row['name_friend']
 
-    # Проверка наличия значения ID_friends
-    if pd.notna(id_fr_value):
-        # Итерация по всем строкам и сравнение значения ID_friends со значениями в столбце friends
-        for idx, r in final_data.iterrows():
-            friends_value = r['friends']
-            if id_fr_value == friends_value:
-                # Копирование значения friends в столбец id_fr
-                final_data.at[index, 'id_fr'] = id_fr_value
-                break  # Прекращаем цикл после первого совпадения
+    # Проверка наличия значения name_friend
+    if pd.notna(name_fr_value):
+        # Поиск индекса строки, в которой содержится значение name_friend в столбце friends
+        friends_index = final_data[final_data['friends'] == name_fr_value].index
+        if len(friends_index) > 0:
+            # Копирование значения name_friend в столбец name_fr в найденной строке
+            final_data.at[friends_index[0], 'name_fr'] = name_fr_value
+
+# Итерация по каждой ячейке столбца phone
+for index, row in final_data.iterrows():
+    phone_fr_value = row['phone']
+
+    # Проверка наличия значения phone
+    if pd.notna(phone_fr_value):
+        # Поиск индекса строки, в которой содержится значение phone в столбце friends
+        friends_index = final_data[final_data['friends'] == phone_fr_value].index
+        if len(friends_index) > 0:
+            # Копирование значения phone в столбец phone_fr в найденной строке
+            final_data.at[friends_index[0], 'phone_fr'] = phone_fr_value
 
 # Сохранение результата в файл final.csv
 final_data.to_csv('final.csv', index=False)
